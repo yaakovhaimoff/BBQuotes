b092b182b6e8ae5920b778e9bd90a868ae349976
Yaakov Haimoff <hyaakov@aol.com>
2025-11-10 17:31:10 +0200

    Initial Commit

---
+++ BBquotes/BBquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBquotesApp.swift
+//  BBquotes
+//
+//  Created by Yaakov Haimoff on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BBquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBquotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBquotes
+//
+//  Created by Yaakov Haimoff on 10.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
